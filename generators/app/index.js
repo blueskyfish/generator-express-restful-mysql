22ef4e9fb4040cc87acb1c22e6a70157a8f95d0e
BlueSkyFish <blueskyfish@kirchnerei.de>
2016-02-28 10:42:06 +0100

    initial commit with the scaffolding

---
+++ generators/app/index.js
@@ -0,0 +1,9 @@
+/*
+ * Generator Express Restful MySQL - https://github.com/blueskyfish/generator-express-restful-mysql.git
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2016 BlueSkyFish
+ */
+
+'use strict';
+
